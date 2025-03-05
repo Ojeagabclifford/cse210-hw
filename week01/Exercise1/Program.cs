2290454292f00f094db985780cd3afa50b7f08b3
ojeagaclifford <cli24003@byui.edu>
2025-03-05 08:59:45 +0100

    my week 01 assignemtn about if statement and conditions

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Writting and display my code which will display first name, last name and first name.
+        
+
+        // Giving a user prompt What is your Frist Name?
+        Console.Write("What is your Frist Name? ");
+        // getting the user first name from the console
+        string fName = Console.ReadLine();
+
+        // Giving a user prompt What is your Last Name?
+        Console.Write("What is your Last Name? ");
+
+        // getting the user last name from the console
+        string lName = Console.ReadLine();
+        
+        // displaying the user name to the console 
+        Console.WriteLine($"your name is {fName},{lName} {fName}.");
+
+
+
+
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,64 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // building a program that ask the user their grade numer and displaying to them if they have pass or not
+        //  Asking user for a propmt
+        string gradeLetter ="";
+        Console.Write("What is your grade Number? ");
+        // gettng the user respone and saveing it in a variable name userGrade
+        string userGrade = Console.ReadLine();
+        // converting the varible value to string to int 
+        int userGradeNumber = int.Parse(userGrade);
+
+        // getting the user last digit number
+        int lastDigit = userGradeNumber % 10;
+        // setting the variable to put my grade sign
+        string latterSign = "";
+
+        
+        // starting up the conditional statement to see if the user input hads pass
+
+        if (userGradeNumber >= 90)
+        {
+            gradeLetter ="A";
+        }
+        else if (userGradeNumber >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (userGradeNumber >= 70)
+        {
+           gradeLetter = "C";
+        }
+        else if (userGradeNumber >= 60)
+        {
+           gradeLetter ="D";
+        }
+       else if (userGradeNumber < 60)
+        {
+           gradeLetter = "F";
+        }
+        else 
+        {
+           Console.WriteLine("You have enter the wrong grade");
+        }
+    //  setting the condition to get the sign
+       if (lastDigit >= 7 || userGradeNumber == 100)
+       {
+        latterSign = "+";
+
+       }
+       else if (lastDigit >=3)
+       {
+        latterSign = "-";
+       }
+       else if ( userGradeNumber < 60)
+       {
+        latterSign ="";
+       }
+       
+        
+        // display the grade letter in the console
+        Console.WriteLine(gradeLetter + latterSign);
     }
 }
