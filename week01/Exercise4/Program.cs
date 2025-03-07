fa211629407a493c885ab6740d14e62ec6985244
ojeagaclifford <cli24003@byui.edu>
2025-03-07 09:48:22 +0100

    This is my week 5 assingnment the program deals on dispalying the name of the user their favourite number and the squre of there number thiis assignment is done by using the input function and the output function

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,100 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // creating a program that store a list of numbers and display it total sum and average find the maximum or largest number in the list.
+
+        // creating an object of a new list to store the user numbers with data type is int
+
+        List<int> numbers = new List<int>();
+
+        int stopNumbers;
+
+        //  creating a while loop to stop when every a user type O
+        Console.WriteLine("Enter a list of number, type ) when finished.");
+
+        do
+        {
+            // creating a prompt 
+            Console.Write("Enter number: ");
+            
+            stopNumbers = int.Parse(Console.ReadLine());
+            numbers.Add(stopNumbers);
+            if(stopNumbers == 0)
+            {
+                // this will remove the number 0 in my list 
+                numbers.Remove(0);
+            }
+            
+            
+        }
+        while (stopNumbers != 0 );
+        numbers.Sort();
+
+        
+
+        //  creating some variable to stror my totalnumber averagenumber
+        int totalNumbers = 0;
+        float averageNumber = 0;
+        // this variable we help out in checking the number maximu number in the list
+         int maxNumber =0;
+            double closeToZero = 0;
+
+
+        // creating a forech loop to loop through all the items in the list of number
+        foreach(int number in numbers)
+        {
+
+            // calculating the total number to gether 
+            totalNumbers += number;
+            // getting the averageNumber by diving the totalnumbers with the total numbers int the list
+            averageNumber = ((float)totalNumbers) / numbers.Count;
+            // closeToZero = Math.Abs(closeToZero);
+            // closeToZero = double.MaxValue;
+
+           
+
+          
+            // creating a conditional statement to check the highs number
+            if (maxNumber < number)
+            {
+                maxNumber = number;
+
+            }
+
+             if( number > 0 && number < maxNumber)    
+            {
+                closeToZero = number;
+            }
+            // int highest = number.Sort;
+            
+            
+           
+
+            // Console.WriteLine(number);
+
+        }
+        // Console.WriteLine(numbers.First());
+            
+         // displaying the total numbers and the average
+            Console.WriteLine($"The sum is: {totalNumbers}");
+            Console.WriteLine($"the average Number is: {averageNumber}");
+            Console.WriteLine($"The largest number is:{maxNumber}");
+            Console.WriteLine($"The smallest positive number is:{closeToZero}");
+            foreach(int number in numbers){
+                Console.WriteLine(number);
+            }
+
+        
+
+
+        // prompting the user to add to the number list
+
+
+
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,58 @@ using System;
 
 class Program
 {
+    
+    // Working on a program that display a name of a person and it favour number and give the number again an integer value
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+           
+        // stroing the user name in a variable
+        string userName = PromptUserName();
+        // // storing the user number in a variable
+        int userNumber = PromptUserNumber();
+         // storing the square number in a variable
+        int squareNumber = SquareNumber(userNumber);
+
+        // welcome message
+        DisplayWelcomeMessage();
+         DisplayResult(userName, userNumber, squareNumber);
     }
+
+    // function to display welcome message
+    static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+           
+        }
+    // creating the funttion to ask user their name
+        static string PromptUserName()
+            {
+                // promting a user for his name and number
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+            return name;
+
+            }
+
+                // gettting the number from the user
+            static int PromptUserNumber(){
+                Console.Write("What is your favourite number: ");
+                int number = int.Parse(Console.ReadLine());
+                return number;
+            }
+        
+        //  creating a function that get user number and reture squre of thst number
+        static int SquareNumber(int number)
+        {
+            return number * number;
+
+        }
+
+            // creating a function to display the result
+            static void DisplayResult(string name, int number, int squareNumber)
+        {
+            Console.WriteLine($"Hello {name}, your favourite number is {number} and the square of {number} is {squareNumber}");
+        }
 }
